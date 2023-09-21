77573433b709293aead19312e1b197f141fe54ee
santinhoPD <pduarte2023@elam.co.uk>
2023-09-21 15:34:43 +0100

    First push
    mo helped

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //Create a reference to the RigidBody2D so we can manipulate it
+    Rigidbody2D playerObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find the RigidBody2D component that is attached to the same object as this script
+        playerObjects = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Create a 'float' that will be equal to the players horizontal input
+        float movementValueX = Input.GetAxis("Horizintal");
+
+        //Change the X velocity of the RigidBody2D to be equal to the movement value
+        playerObjects.velocity = new Vector2 (movementValueX, playerObjects.velocity.y);
+    }
+}
